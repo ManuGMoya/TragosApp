399899d3d0251bc6ee7221e9d3499e3da3bb75f8
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2020-10-03 21:55:33 +0200

    Initial commit

---
+++ app/src/main/java/com/manugmoya/tragosapp/ui/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.manugmoya.tragosapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.manugmoya.tragosapp.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_go_to_details.setOnClickListener {
+            findNavController().navigate(R.id.tragosdetalleFragment)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/manugmoya/tragosapp/ui/TragosdetalleFragment.kt
@@ -0,0 +1,23 @@
+package com.manugmoya.tragosapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.manugmoya.tragosapp.R
+
+class TragosdetalleFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tragosdetalle, container, false)
+    }
+}
